2480c512b7c6ada3091446d4fac1541661cec5f2
RickT <2274289773@qq.com>
2024-10-15 11:49:25 +0800

    fix getinst.py

--- getinst.py
+++ getinst.py
@@ -1,5 +1,4 @@
 import re
-with open('mycpu_top.v') as f:
-    content = f.read()
-insts = re.findall(r'(inst(_[a-z]+)+) ',content)
-print(' | '.join(set(i[0] for i in insts)))
+insts = re.findall(r'(inst(_[a-z0-9]+)+) ',open('mycpu_top.v').read())
+ckinst = lambda s: not any(i in s for i in ['sram','reg'])
+print(' | '.join(set(i[0] for i in insts if ckinst(i[0]))))
